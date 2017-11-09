766acfaed151ba6e09c1cde10a2276b515aa20e5
Kefeihei <heikefei@foxmail.com>
2017-11-09 13:44:45 +0100

    add function

---
+++ script.py
@@ -0,0 +1,7 @@
+print("hello word")
+
+import txt as tt
+
+a = tt.circle_area(1)
+
+print(a)
---
+++ text.py
@@ -0,0 +1,7 @@
+import numpy as np
+print("hello")
+
+def circle_area(x):
+
+    result = np.pi*(x**2)
+    return result
